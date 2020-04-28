cc61e052a3486ee77d9574a942f7ea3a27f6ce1a
yogevbd <yogev132@gmail.com>
2020-04-28 05:51:17 +0300

    Use github core package

---
+++ src/main.ts
@@ -0,0 +1,33 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+
+async function run() {
+  try {
+    const requiredLabels = core.getInput('valid-labels', {required: true}).split(',');
+    const requiredLabelsAll = core.getInput('valid-labels-all', {required: true}).split(',');
+    const bannedLabels = core.getInput('banned-labels', {required: true}).split(',');
+
+    core.debug(`Verified PR match valid labels: ${requiredLabels}`);
+
+    const labels = github.context!.payload!.pull_request!.labels;
+    console.log(`PR labels: ${JSON.stringify(github.context)}`);
+
+    if (!requiredLabelsAll.every(l => l.includes(l.name))) {
+      core.setFailed(`All labels are required for this PR: ${requiredLabelsAll}`);
+    }
+
+    if (!labels.some(l => requiredLabels.includes(l.name))) {
+      core.setFailed(`Please select one of the required labels for this PR: ${requiredLabels}`);
+    }
+
+    let bannedLabel;
+    if (bannedLabel = labels.find(l => bannedLabels.includes(l.name))) {
+      core.setFailed(`${bannedLabel} label is banned`);
+    }
+
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
